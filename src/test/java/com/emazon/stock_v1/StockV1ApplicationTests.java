ec5f7b9aa16cafb5b80052bf3f27ba77838af663
Gustavo <desarrollador.gustavo@gmail.com>
2024-08-15 14:25:23 -0500

    init stock_v1

---
+++ src/test/java/com/emazon/stock_v1/StockV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.stock_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
